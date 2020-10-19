b68fd951cf9fdb739176a9a47fd055af8b561f3a
Anh Nguyen <anhknguyen97@yahoo.com>
2020-10-19 13:36:27 -0700

    Shorten url and cache into a hashmap

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 	"math/rand"
+	"bufio"
+	"os"
 ) 
 
 var characters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
@@ -16,16 +18,21 @@ func randSeq(n int) string {
 	return string(buffer)
 }
 
-func shortenURL(url string) {
-
-}
-
 func main() {
 	fmt.Println("URL Shortener")
 	url_map := make(map[string] string)
 
-	rand.Seed(time.Now().UnixNano())
-	fmt.Println(randSeq(10))
+	// read url input string
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter URL to shorten: ")
+	text, _  := reader.ReadString('\n')
 
+	// generate random string
+	rand.Seed(time.Now().UnixNano())
+	shortenURL := randSeq(10)
+
+	// place into map
+	url_map[text] = shortenURL
 	fmt.Println("map:", url_map)
+
 }
