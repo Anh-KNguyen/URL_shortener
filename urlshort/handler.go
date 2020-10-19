d59ec8d58283cfac643e2a851bab5299d3aa7487
Anh Nguyen <anhknguyen97@yahoo.com>
2020-10-19 16:00:39 -0700

    File structure for maphandler

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -4,7 +4,7 @@ import "net/http"
 
 // Returns a http.HandlerFunc that maps paths to their corresponding URL (key:val)
 // the fallback http.Handler is called when path doesn't exist
-func MapHandler(pathsToUrls map[string] string, fallback http.Handler) http.HandlerFunc {
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if pathsToUrls[r.URL.Path] != "" {
 			http.Redirect(w, r, pathsToUrls[r.URL.Path], http.StatusFound)
