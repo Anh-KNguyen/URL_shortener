da877b5409893fe7e9697aab70798a50c611f45c
Anh Nguyen <anhknguyen97@yahoo.com>
2020-10-20 04:01:40 -0700

    Create urlshort package

--- main.go
+++ main.go
@@ -1,115 +1,25 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"math/rand"
 	"net/http"
-	"strings"
-	"time"
 
+	"github.com/Anh-KNguyen/URL_shortener/urlshort"
 	"github.com/gorilla/mux"
 )
-
-var characters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-
-var pathsToURL = make(map[string]string)
-
-type Url struct {
-	LongUrl  string `json:"long_url"`
-	ShortUrl string `json:"short_url"`
-}
 
 // define Rest APIs and Handlers
 func defaultMux() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/links", InputHandler).Methods(http.MethodPost)
-	r.HandleFunc("/links/{id}", OutputHandler).Methods(http.MethodGet)
-	r.HandleFunc("/{id}", PathHandler)
-	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/links", urlshort.InputHandler).Methods(http.MethodPost)
+	r.HandleFunc("/links/{id}", urlshort.OutputHandler).Methods(http.MethodGet)
+	r.HandleFunc("/{id}", urlshort.PathHandler)
+	r.HandleFunc("/", urlshort.HomeHandler)
 
 	return r
-}
-
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Home Page")
-}
-
-func InputHandler(w http.ResponseWriter, r *http.Request) {
-	// define new Url struct
-	var u Url
-
-	// decode the request body into Url struct
-	err := json.NewDecoder(r.Body).Decode(&u)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// generate random short_url
-	rand.Seed(time.Now().UnixNano())
-	u.ShortUrl = randSeq(10)
-
-	// place into map
-	pathsToURL[u.ShortUrl] = u.LongUrl
-
-	// encode struct into json string representation
-	err = json.NewEncoder(w).Encode(&u)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func OutputHandler(w http.ResponseWriter, r *http.Request) {
-	// retrieve route
-	vars := mux.Vars(r)
-	shortId := vars["id"]
-	longId := pathsToURL[shortId]
-
-	// store retrieved urls into Url struct
-	u := Url{
-		ShortUrl: shortId,
-		LongUrl:  longId,
-	}
-
-	err := json.NewEncoder(w).Encode(u)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func PathHandler(w http.ResponseWriter, r *http.Request) {
-	// retrieve route
-	vars := mux.Vars(r)
-	shortId := vars["id"]
-	longId := pathsToURL[shortId]
-
-	if longId == "" {
-		http.Error(w, shortId+" not found", http.StatusNotFound)
-		return
-	}
-
-	// redirect to long_url
-	if !strings.HasPrefix(longId, "http://") {
-		longId = "http://" + longId
-	}
-	http.Redirect(w, r, longId, http.StatusMovedPermanently)
-}
-
-// generate random string for short_url
-func randSeq(n int) string {
-	buffer := make([]byte, n)
-	for i := range buffer {
-		buffer[i] = characters[rand.Intn(len(characters))]
-	}
-	return string(buffer)
 }
 
 func main() {
 	// start http server
 	mux := defaultMux()
 	_ = http.ListenAndServe(":8080", mux)
-
 }
--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,14 +1,97 @@
 package urlshort
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
 
-// Returns a http.HandlerFunc that maps paths to their corresponding URL (key:val)
-// the fallback http.Handler is called when path doesn't exist
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if pathsToUrls[r.URL.Path] != "" {
-			http.Redirect(w, r, pathsToUrls[r.URL.Path], http.StatusFound)
-		}
-		fallback.ServeHTTP(w, r)
-	})
+	"github.com/gorilla/mux"
+)
+
+var Characters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+var PathsToURL = make(map[string]string)
+
+type Url struct {
+	LongUrl  string `json:"long_url"`
+	ShortUrl string `json:"short_url"`
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Home Page")
+}
+
+func InputHandler(w http.ResponseWriter, r *http.Request) {
+	// define new Url struct
+	var u Url
+
+	// decode the request body into Url struct
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// generate random short_url
+	rand.Seed(time.Now().UnixNano())
+	u.ShortUrl = randSeq(10)
+
+	// place into map
+	PathsToURL[u.ShortUrl] = u.LongUrl
+
+	// encode struct into json string representation
+	err = json.NewEncoder(w).Encode(&u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func OutputHandler(w http.ResponseWriter, r *http.Request) {
+	// retrieve route
+	vars := mux.Vars(r)
+	shortId := vars["id"]
+	longId := PathsToURL[shortId]
+
+	// store retrieved urls into Url struct
+	u := Url{
+		ShortUrl: shortId,
+		LongUrl:  longId,
+	}
+
+	err := json.NewEncoder(w).Encode(u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func PathHandler(w http.ResponseWriter, r *http.Request) {
+	// retrieve route
+	vars := mux.Vars(r)
+	shortId := vars["id"]
+	longId := PathsToURL[shortId]
+
+	if longId == "" {
+		http.Error(w, shortId+" not found", http.StatusNotFound)
+		return
+	}
+
+	// redirect to long_url
+	if !strings.HasPrefix(longId, "http://") {
+		longId = "http://" + longId
+	}
+	http.Redirect(w, r, longId, http.StatusMovedPermanently)
+}
+
+// generate random string for short_url
+func randSeq(n int) string {
+	buffer := make([]byte, n)
+	for i := range buffer {
+		buffer[i] = Characters[rand.Intn(len(Characters))]
+	}
+	return string(buffer)
 }
